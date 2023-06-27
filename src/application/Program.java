1051f1c900d0bc3da0110b03d8b01fa66905abde
Leonidas Marques <leonidasmmarques@outlook.com>
2023-06-27 14:39:11 -0300

    iniciando classe statica

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,29 @@
 package application;
 
+import java.util.Scanner;
+
+import util.Conversor;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		
+		System.out.println("Informe a cotação do dolar: ");
+		double qtdDolar = sc.nextDouble();
+		
+		System.out.println("Informe qauntos dolares quer comprar: ");
+		double cotacao = sc.nextDouble();
+		
+		double cota = Conversor.converter(cotacao, qtdDolar);
+		
+		System.out.println(cota);
+		
+		
+		
+		
+		sc.close();
 
 	}
 
--- src/util/Conversor.java
+++ src/util/Conversor.java
@@ -1,5 +1,17 @@
 package util;
 
 public class Conversor {
-
+	
+	public double cotacaoDolar;
+	public double dolar;
+	public double soma;
+	public double taxa;
+	
+	public static double converter(double a, double b) {
+		return a * b;
+	}
+	
+	
+	
+	
 }
