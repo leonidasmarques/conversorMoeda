56391df8a215bafae8497b0ef35c1cf89b70fedc
Leonidas Marques <leonidasmmarques@outlook.com>
2023-06-27 14:48:31 -0300

    retirando variavel desnecessaria

--- src/util/Conversor.java
+++ src/util/Conversor.java
@@ -2,9 +2,6 @@ package util;
 
 public class Conversor {
 	
-	public static double cotacaoDolar;
-	public static double dolar;
-	public static double soma;
 	public static double taxa;
 	
 	public static double converter(double a, double b) {
